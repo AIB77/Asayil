6f9f51a516cfe483a643a5cc362057d8e18d5301
AIB77 <90730281+AIB77@users.noreply.github.com>
2021-09-20 15:43:37 +0300

    Add files via upload
    The Top 5 movies

--- main.kt
+++ main.kt
@@ -1,6 +1,26 @@
-fun main()
-{
-println("Enter Your Name:")
-    val name= readLine()
-    println("Hello "+name+", Welcome to my program")
+fun main() {
+    val moviearray = arrayOf(" ", " ", " ", " ", " ")
+
+    print("Enter a name of movie 1th:")
+    var m1 = readLine()!!.toString()
+    moviearray[0] = "$m1"
+    print("Enter a name of movie 2th:")
+    var m2 = readLine()!!.toString()
+    moviearray[1] = "$m2"
+    print("Enter a name of movie 3th:")
+    var m3 = readLine()!!.toString()
+    moviearray[2] = "$m3"
+    print("Enter a name of movie 4th:")
+    var m4 = readLine()!!.toString()
+    moviearray[3] = "$m4"
+    print("Enter a name of movie 5th:")
+    var m5 = readLine()!!.toString()
+    moviearray[4] = "$m5"
+
+
+    println("your Top 5 Movies are:")
+    for (item in moviearray)
+        println(item)
+
 }
+
